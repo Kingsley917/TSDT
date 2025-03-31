1aef734c9d4dc30022c08e9603d31c8c336e3b0c
Kingsley917 <2417308709@qq.com>
2025-03-31 11:10:12 +0800

    message 9: After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,30 +12,37 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
+    
         # 张三听说有一个在线待办事项的应用
         # 他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
-
+    
         # 他注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  # (1)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
-
+    
         # 应用有一个输入待办事项的文本输入框
-        textbox = self.browser.find_element(By.ID, 'id_new_item')  # (1)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item') #(1)
         self.assertEqual(
-            textbox.get_attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-
+    
         # 他在文本输入框中输入了“Buy flowers”
-        textbox.send_keys('Buy flowers')  # (2)
-
-        # 他按下了回车键后，页面更新了
+        inputbox.send_keys('Buy flowers') #(2)
+    
+        # 他按了回车键键后，页面更新了
         # 待办事项表格中显示了“1: Buy flowers”
-        textbox.send_keys(Keys.ENTER)  # (3)
-        time.sleep(1)  # (4)
+        inputbox.send_keys(Keys.ENTER) #(3)
+        time.sleep(1) #(4)
+        self.check_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他输入了“Give a gift to Lisi”
@@ -45,16 +52,12 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
 
         # 张三想知道这个网站是否会记住她的清单
         # 他看到网站为他生成了一个唯一的URL
         self.fail('Finish the test!')
-
-        # 页面再次更新，她的清单中显示了这两个待办事项
 
 if __name__ == '__main__':
     unittest.main()
