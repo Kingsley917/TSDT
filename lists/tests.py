90751f2dd71c2ae43b8a61171e77fb1a89a32dd5
Kingsley917 <2417308709@qq.com>
2025-05-06 11:52:50 +0800

    message 17: change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -2,38 +2,14 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-from django.template.loader import render_to_string
-from lists.models import Item
-
-class ItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first list item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
-
-
-from django.test import TestCase
-from .models import Item
+from lists.models import Item, List  # 导入 List 模型
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')   
-        
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -41,13 +17,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/lists/the-new-page/')
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
+        list_user = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_user)
+        Item.objects.create(text='itemey 2', list=list_user)
         
 class NewListTest(TestCase):
 
@@ -60,3 +32,32 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-new-page/')
+
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_user = List()
+        list_user.save()
+
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.list = list_user
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_user
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_user)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list, list_user)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_user)
